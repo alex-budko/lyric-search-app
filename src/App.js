52fa0420e7d1bc788fc576833dc4c809f18f4e05
alex-budko <xalex.budko@gmail.com>
2022-05-27 16:57:11 -0400

    implemented Index and Tracks components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import React from 'react'
+
 import Navbar from './components/layout/Navbar.js'
+import Index from './components/layout/Index.js'
+
+
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-    </div>
+    <Router>
+      <React.Fragment>
+        <Navbar />
+        <div className='container'>
+          <Routes>
+            <Route exact path='/' component={Index} />
+          </Routes>
+        </div>
+      </React.Fragment>
+    </Router>
   );
 }
 
