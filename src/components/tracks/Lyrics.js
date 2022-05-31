a77559fe6fcdfda749035dbdd8e5159db1d7937b
alex-budko <xalex.budko@gmail.com>
2022-05-31 16:25:49 -0400

    changed Lyrics component

--- src/components/tracks/Lyrics.js
+++ src/components/tracks/Lyrics.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import { useParams } from 'react-router-dom';
 import axios from 'axios'
 
 class Lyrics extends Component {
@@ -6,14 +7,13 @@ class Lyrics extends Component {
         track: {},
         lyrics: {}
     }
+    
     componentDidMount() {
         axios
         .get(`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${
             this.props.id}&apikey=${
             process.env.REACT_APP_MM_KEY}`)
         .then(res => {
-            console.log(this.props.id) 
-            // this.props.id fix
             console.log(res.data)
             this.setState({track_list: res.data.message.body.track_list})
         })
